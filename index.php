a63326d5ffff79efccf85356b0672dd8457ef3b3
Jubayerahmaad <jubuofficial84@gmail.com>
2024-12-16 00:36:30 +0600

    Variable & Data Type & Error

--- index.php
+++ index.php
@@ -1,3 +1,25 @@
 <?php
- echo 'hello world';
+//  echo 'hello world';
+// variable
+// Data type
+// Error
+
+/* $username = 'Hello World';
+
+ echo $username; */
+
+//  const username = 'Hi jhon';
+
+//  echo username;
+
+$username = 'Jhon Deo';
+$number = 76;
+$number2 = 1.5;
+$isEnrolled = true / false;
+$students = ['Jubayer', 'Habib'];
+
+$null = null;
+
+var_dump ($username);
+
 ?>
