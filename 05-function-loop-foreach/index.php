38948bd4260e4cc7116f6f955a9dfeb78fe7a398
Jubayerahmaad <jubuofficial84@gmail.com>
2024-12-18 01:57:53 +0600

    Functions For & Foreach Loop

---
+++ 05-function-loop-foreach/index.php
@@ -0,0 +1,36 @@
+<?php
+// Function
+// Loop 
+// For Each Loop
+
+function sayHello(){
+    return 'Hi There <br>';
+}
+
+sayHello();
+
+
+// Loop 
+
+for($i = 0 ; $i < 10 ; $i++){
+    echo $i ." ". sayHello();
+}
+
+// For Each Loop 
+
+$languages = ['HTML' ,'CSS' , 'JAVASCRIPT'];
+
+foreach( $languages as $item){
+    echo $item . '<br>';
+}
+
+// Function With Argument 
+
+function sayGoodMorging($name){
+    echo 'Good Morning,' . " " . $name;
+
+}
+sayGoodMorging('Abdullah');
+
+
+?>
