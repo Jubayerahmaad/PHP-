5af8f689ba6aeaa277fd3d7652dc98b689961add
Jubayerahmaad <jubuofficial84@gmail.com>
2025-01-04 16:41:07 +0600

    if else

--- 06-condition-and-layout/index.php
+++ 06-condition-and-layout/index.php
@@ -8,16 +8,19 @@
 //     echo "You are over 20.";
 // }
 
-$userAge = 10;
+$userAge = 26;
 
-if ($userAge == 20) {
-    echo "You Are Under 20";
-} elseif($userAge == 30) {
-    echo "You are over 30.";
-} elseif($userAge == 40){
-    echo "You are 40";
-} else{
-    echo "undifined";
-};
+// if ($userAge == 20) {
+//     echo "You Are Under 20";
+// } elseif($userAge == 30) {
+//     echo "You are over 30.";
+// } elseif($userAge == 40){
+//     echo "You are 40";
+// } else{
+//     echo "undifined";
+// };
 
+if ($userAge > 20 && $userAge < 30){
+    echo 'You are Accepted!';
+}
 ?>
