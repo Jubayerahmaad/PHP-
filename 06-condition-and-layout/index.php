3a698e47db5b061bc454281ddc8c2c86ce080704
Jubayerahmaad <jubuofficial84@gmail.com>
2025-01-10 18:37:55 +0600

    Form 1

--- 06-condition-and-layout/index.php
+++ 06-condition-and-layout/index.php
@@ -8,7 +8,7 @@
 //     echo "You are over 20.";
 // }
 
-$userAge = 26;
+// $userAge = 2;
 
 // if ($userAge == 20) {
 //     echo "You Are Under 20";
@@ -20,7 +20,32 @@ $userAge = 26;
 //     echo "undifined";
 // };
 
-if ($userAge > 20 && $userAge < 30){
-    echo 'You are Accepted!';
-}
+// if ($userAge > 20 && $userAge < 30){
+//     echo 'You are Accepted!';
+// }
+
+// login logged out
+
+// $isLogging = true;
+
+// if($isLogging){
+//     echo 'You are loggin';
+//     echo '<button>Logout</button>';
+// } else{
+//     echo 'You are logged Out';
+//     echo '<button>Login</button>';
+// }
+
+/* 
+
+Include files
+include('layout/header.php');
+echo 'My Home Page';
+include('layout/footer.php');
+
+*/
+
+// redirect 
+header('Location: 06-condition-and-layout/layout/login.php');
+
 ?>
---
+++ 07-form/index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="Form-submit.php">
+        <input type="text" placeholder="Name" name='username'> <br>  <br>
+        <input type="Email" placeholder="Email" name='email'> <br>  <br>
+        <input type="submit" value="Submit Form">
+    </form>
+</body>
+</html>
