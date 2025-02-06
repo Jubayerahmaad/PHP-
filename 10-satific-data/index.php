0f189c9779e701b9c624025cbcc919ca4258b117
Jubayerahmaad <jubuofficial84@gmail.com>
2025-02-06 23:19:39 +0600

    10-Spacific-Data

--- 10-satific-data/index.php
+++ 10-satific-data/index.php
@@ -1,11 +1,18 @@
 <?php
 
-
-
-
-
-
-
-
+// Load the database
+include('select.php');
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
---
+++ 10-satific-data/select.php
@@ -0,0 +1,24 @@
+<?php
+
+// Database info
+$host = 'localhost';
+$db_user = 'root';
+$db_pass = '';
+$db_name = 'dataofcustomer';
+
+// DB connection
+$conn = mysqli_connect($host, $db_user, $db_pass, $db_name);
+
+// Query 
+$user_id =  $_REQUEST['id'];
+
+$query = "SELECT * FROM customersdata WHERE id = '$user_id' ";
+
+// Run the query 
+$result = mysqli_query($conn, $query);
+
+// Result
+// while( $item = mysqli_fetch_assoc($result) ){
+// 	echo $item['name'];
+// }
+
